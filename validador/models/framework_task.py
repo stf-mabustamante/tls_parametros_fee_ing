77fe13d39444b1c745967a4363afe5a6d55e8865
stf-mabustamante <mabustamante@stefanini.com>
2026-05-11 12:59:50 -0400

    Delete validador/models directory

--- validador/models/framework_task.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class FrameworkTask:
-    task_name: str
-    params: dict
--- validador/models/manifest.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-from models.repo_object import RepoObject
-
-
-@dataclass
-class Manifest:
-    objetos: List[RepoObject]
