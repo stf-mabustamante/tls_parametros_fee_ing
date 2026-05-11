1d9d60ff8be4a81300f3d84934ab59b36dd2a4a1
stf-mabustamante <mabustamante@stefanini.com>
2026-05-11 18:48:21 -0400

    Create framework_validator.py

---
+++ validator/validators/framework_validator.py
@@ -0,0 +1,30 @@
+from utils.dict_utils import get_nested
+
+
+class FrameworkValidator:
+
+    def validate(self, task, task_definition):
+
+        errors = []
+
+        required_params = task_definition.get(
+            'required_params',
+            []
+        )
+
+        for required_param in required_params:
+
+            value = get_nested(
+                task.params,
+                required_param
+            )
+
+            if value is None:
+
+                errors.append(
+                    f'Task [{task.task_name}] '
+                    f'no tiene parámetro requerido: '
+                    f'{required_param}'
+                )
+
+        return errors
