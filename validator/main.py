5789542f20b0135a7311919ad72ae94004eb0a4a
stf-mabustamante <mabustamante@stefanini.com>
2026-05-12 12:51:36 -0400

    Update main.py

--- validator/main.py
+++ validator/main.py
@@ -67,7 +67,7 @@ for ini_file in ini_files:
     tasks = IniParser().parse(
         str(ini_file)
     )
-    print("task:",task)
+    print("tasks:",tasks)
     for task in tasks:
 
         task_definition = catalog[
